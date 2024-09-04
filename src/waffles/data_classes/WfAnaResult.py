b305846741d259673a59e0ef9587329c643cfae0
Julio <jurenag@outlook.es>
2024-09-04 14:21:47 +0200

    Abide by PEP8 in WfAnaResult.py

--- src/waffles/data_classes/WfAnaResult.py
+++ src/waffles/data_classes/WfAnaResult.py
@@ -2,9 +2,7 @@ from waffles.data_classes.ORDict import ORDict
 
 
 class WfAnaResult(ORDict):
-
-    """
-    Stands for Waveform Analysis Result. This class
+    """Stands for Waveform Analysis Result. This class
     inherits from the ORDict class, and it is intended
     to store the results of an analysis (i.e. a class
     which derives from WfAna) which has been performed
