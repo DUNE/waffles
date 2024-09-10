109f15445b999c27a6b343e08755ec326ae71aa0
Julio <jurenag@outlook.es>
2024-09-10 14:24:01 +0200

    Abide by PEP8 in np04_utils/utils.py
    This file was not taken into account in the big PEP8-compliance pull request because it was introduced in a branch which diverged from main before marroyav_reformatting did

--- src/waffles/np04_utils/utils.py
+++ src/waffles/np04_utils/utils.py
@@ -3,10 +3,10 @@ from waffles.data_classes.ChannelMap import ChannelMap
 from waffles.np04_data.ProtoDUNE_HD_APA_maps import flat_APA_map
 from waffles.Exceptions import GenerateExceptionMessage
 
-def get_channel_iterator(   apa_no : ChannelMap,
-                            endpoint : int,
-                            channel : int):
-    
+def get_channel_iterator(   
+    apa_no: ChannelMap,
+    endpoint: int,
+    channel: int):
     """
     This function returns the iterator value of the given
     channel in the flattened map of the specified APA.
@@ -16,16 +16,16 @@ def get_channel_iterator(   apa_no : ChannelMap,
 
     Parameters
     ----------
-    apa_no : int
+    apa_no: int
         The APA number
-    endpoint : int
+    endpoint: int
         The endpoint number
-    channel : int
+    channel: int
         The channel number
 
     Returns
     -------
-    iterator : int
+    iterator: int
         The iterator value of the given channel in the
         flattened map of the specified APA
     """
@@ -34,15 +34,19 @@ def get_channel_iterator(   apa_no : ChannelMap,
         flat_map = flat_APA_map[apa_no]
 
     except KeyError:
-        raise Exception(GenerateExceptionMessage( 1,
-                                                    'get_channel_iterator()',
-                                                    f"The given APA number ({apa_no}) is not recognised."))
+        raise Exception(GenerateExceptionMessage( 
+            1,
+            'get_channel_iterator()',
+            f"The given APA number ({apa_no}) is not recognised."))
+    
     iterator = 0
     
     if flat_map.rows == 1:
         for j in range(flat_map.columns):
 
-            if flat_map.data[0][j].endpoint == endpoint and flat_map.data[0][j].channel == channel:
+            if flat_map.data[0][j].endpoint == endpoint and \
+                flat_map.data[0][j].channel == channel:
+                    
                     return iterator
             else:
                 iterator += 1
@@ -50,15 +54,20 @@ def get_channel_iterator(   apa_no : ChannelMap,
     elif flat_map.columns == 1:
          for i in range(flat_map.rows):
 
-            if flat_map.data[i][0].endpoint == endpoint and flat_map.data[i][0].channel == channel:
+            if flat_map.data[i][0].endpoint == endpoint and \
+                flat_map.data[i][0].channel == channel:
+                    
                     return iterator
             else:
                 iterator += 1
     else:
-        raise Exception(GenerateExceptionMessage( 2,
-                                                    'get_channel_iterator()',
-                                                    f"The retrieved map is not flat."))
+        raise Exception(GenerateExceptionMessage(
+            2,
+            'get_channel_iterator()',
+            f"The retrieved map is not flat."))
     
-    raise Exception(GenerateExceptionMessage( 3,
-                                                'get_channel_iterator()',
-                                                f"The given channel ({endpoint}-{channel}) is not present in the specified APA."))
+    raise Exception(GenerateExceptionMessage(
+        3,
+        'get_channel_iterator()',
+        f"The given channel ({endpoint}-{channel}) "
+        "is not present in the specified APA."))
