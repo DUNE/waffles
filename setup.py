a6c7e7b2f349a77b42f5c45a5e1cdb740c012a80
Sam Fogarty <samuel.fogarty@colostate.edu>
2024-09-22 03:39:10 +0200

    Include bin dir

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     description='PDS Waveform Analysis Framework For Light Emission Studies',
     author='Many',
     package_dir={"": "src"},
-    packages=find_packages(where='src')
+    packages=find_packages(where='src'),
+    package_data={'bin': ['*csv', '*npy', '*npz']}
     # ,
     # install_requires=['numpy','dash','dash_bootstrap_components','graphviz','h5py','inquirer','jupyter','matplotlib','rich','scipy','pandas','plotly','uproot','kaleido','numba']
 )
