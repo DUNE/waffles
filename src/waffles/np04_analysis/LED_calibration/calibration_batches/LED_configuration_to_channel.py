94f96845021de3a3b1aa4f2eeea0cc4e1d6c70c6
Julio <jurenag@outlook.es>
2024-10-29 18:06:36 +0100

    Minor changes to the LED-configs-to-channel mappings files

--- src/waffles/np04_analysis/LED_calibration/calibration_batches/LED_configuration_to_channel.py
+++ src/waffles/np04_analysis/LED_calibration/calibration_batches/LED_configuration_to_channel.py
@@ -6,14 +6,15 @@ from waffles.np04_analysis.LED_calibration.calibration_batches.batch_2.LED_confi
 from waffles.np04_analysis.LED_calibration.calibration_batches.batch_3.LED_configuration_to_channel \
     import config_to_channels as batch_3
 
-config_to_channels = {} # run_to_config is a 6-levels nested dictionary where:
-                        # - the first key level is an integer which labels a certain measurements batch
-                        # - the second key level is the APA number (1,2,3,4)
-                        # - the third key level is the PDE value (0.4, 0.45, 0.5)
-                        # - the fourth key level is the LED configuration
-                        #     - an LED configuration is a 3-tuple whose format is (channel_mask, ticks_width, pulse_bias_percent)
-                        # - the fifth key level is the endpoint number
-                        # - the sixth key level is a list of channels for the given endpoint
+# run_to_config is a 6-levels nested dictionary where:
+# - the first key level is an integer which labels a certain measurements batch
+# - the second key level is the APA number (1,2,3,4)
+# - the third key level is the PDE value (0.4, 0.45, 0.5)
+# - the fourth key level is the LED configuration
+#     - an LED configuration is a 3-tuple whose format is (channel_mask, ticks_width, pulse_bias_percent)
+# - the fifth key level is the endpoint number
+# - the sixth key level is a list of channels for the given endpoint
+config_to_channels = {}
 
 # P.e. config_to_channels[1][3][0.45][(1 , 1, 1400)][111] gives the channels of endpoint 111 from APA 3 for which 
 # the data taken with the LED configuration (1 , 1, 1400) during the first measurement batch should be used.
--- src/waffles/np04_analysis/LED_calibration/calibration_batches/batch_3/LED_configuration_to_channel.py
+++ src/waffles/np04_analysis/LED_calibration/calibration_batches/batch_3/LED_configuration_to_channel.py
@@ -1,10 +1,11 @@
-config_to_channels = {}     # config_to_channels is a 5-levels nested dictionary where:
+# config_to_channels is a 5-levels nested dictionary where:
 # - the first key level is the APA number (1,2,3,4)
 # - the second key level is the PDE value (0.4, 0.45, 0.5)
 # - the third key level is the LED configuration
 #     - an LED configuration is a 3-tuple whose format is (channel_mask, ticks_width, pulse_bias_percent)
 # - the fourth key level is the endpoint number
 # - the fifth key level is a list of channels for the given endpoint
+config_to_channels = {}
 
 # P.e. config_to_channels[2][0.45][(50, 20, 2200)] gives the endpoints and channels from APA 2 at 0.45
 # PDE which should be calibrated using the data collected using the LED configuration (50, 20, 2200)
