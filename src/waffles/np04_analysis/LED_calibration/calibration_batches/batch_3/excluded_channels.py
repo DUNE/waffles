5c95ed44ae5fa58b3c3153f7e8fe6ca59aff732f
Julio <jurenag@outlook.es>
2024-10-29 18:02:33 +0100

    PEP8 changes to the excluded-channels files

--- src/waffles/np04_analysis/LED_calibration/calibration_batches/batch_3/excluded_channels.py
+++ src/waffles/np04_analysis/LED_calibration/calibration_batches/batch_3/excluded_channels.py
@@ -1,8 +1,9 @@
-excluded_channels = {}  # excluded_channels is a 4-levels nested dictionary where:
+# excluded_channels is a 4-levels nested dictionary where:
 # - the first key level is the APA number (1,2,3,4)
 # - the second key level is the PDE value (0.4, 0.45, 0.5)
 # - the third key level is the endpoint
 # - the fourth key level is a list of (excluded) channels
+excluded_channels = {}  
 
 # In the third calibration batch (29-30/07/2024), APA 1 data was not acquired
 for apa in range(2, 5):
@@ -11,25 +12,43 @@ for apa in range(2, 5):
     for pde in [ 0.4, 0.45, 0.5 ]:
         excluded_channels[apa][pde] = {}
 
-excluded_channels[2][0.40] = {109: [17, 11, 13, 14, 16, 10]}
+excluded_channels[2][0.40] = {
+    109: [17, 11, 13, 14, 16, 10]
+}
 
-excluded_channels[2][0.45] = {109: [17, 11, 13, 14, 16, 10]}
+excluded_channels[2][0.45] = {
+    109: [17, 11, 13, 14, 16, 10]
+}
 
-excluded_channels[2][0.50] = {109: [17, 11, 13, 14, 16, 10]}
-
-
-excluded_channels[3][0.40] = {111: []}
-
-excluded_channels[3][0.45] = {111: []}
-
-excluded_channels[3][0.50] = {111: []}
+excluded_channels[2][0.50] = {
+    109: [17, 11, 13, 14, 16, 10]
+}
 
 
-excluded_channels[4][0.40] = {112: [], 
-                              113: []}
+excluded_channels[3][0.40] = {
+    111: []
+}
 
-excluded_channels[4][0.45] = {112: [], 
-                              113: []}
+excluded_channels[3][0.45] = {
+    111: []
+}
 
-excluded_channels[4][0.50] = {112: [], 
-                              113: []}
+excluded_channels[3][0.50] = {
+    111: []
+}
+
+
+excluded_channels[4][0.40] = {
+    112: [], 
+    113: []
+}
+
+excluded_channels[4][0.45] = {
+    112: [], 
+    113: []
+}
+
+excluded_channels[4][0.50] = {
+    112: [], 
+    113: []
+}
