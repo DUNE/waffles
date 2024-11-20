506e96b556a6ad88dca77afc6dc032f17fc21ff9
Julio <jurenag@outlook.es>
2024-11-20 16:18:39 +0100

    Substitute the WaveformAdcs.__truncate_adcs() method with __slice_adcs()
    The new method allows the selection of the end point of the truncation/slice, which is convenient for the homogeneous-slicing of a WaveformSet which will be introduced in the near future.

--- src/waffles/data_classes/WaveformAdcs.py
+++ src/waffles/data_classes/WaveformAdcs.py
@@ -112,7 +112,6 @@ class WaveformAdcs:
 #       self.__time_step_ns = input
 #       return
 
-
     def __set_time_offset(self, input: float) -> None:
         """This method is not intended for user usage. It 
         is a setter for the time_offset attribute.
@@ -141,29 +140,34 @@ class WaveformAdcs:
 
         return
 
-    def __truncate_adcs(
-        self, 
-        number_of_points_to_keep: int
+    def __slice_adcs(
+        self,
+        start: int,
+        end: int
     ) -> None:
-        """This method is not intended for user usage. It 
-        truncates the self.__adcs attribute array to the 
-        first 'number_of_points_to_keep' points.
+        """This method is not intended for user usage.
+        No well-formedness checks are performed here.
+        This method slices the self.__adcs attribute
+        array to the [start, end] interval. This method
+        applies the change in place.
 
         Parameters
         ----------
-        number_of_points_to_keep: int
+        start: int
+            Iterator value for the (inclusive) start of
+            the slice
+        end: int
+            Iterator value for the (exclusive) end of
+            the slice
 
         Returns
         ----------
         None
         """
 
-        # Numpy handles the case where number_of_points_to_keep
-        # is greater than the length of self.__adcs.
-        self.__adcs = self.__adcs[:number_of_points_to_keep]
+        self.__adcs = self.__adcs[start:end]
         return
         
-
     def confine_iterator_value(self, input: int) -> int:
         """Confines the input integer to the range [0, len(self.__adcs) - 1].
         I.e returns 0 if input is negative, returns input if input belongs
