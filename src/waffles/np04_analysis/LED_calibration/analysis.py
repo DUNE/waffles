c1b66dbaabeb9521ab756c7453d31a162f2b1e5f
Anselmo Cervera Villanueva <acervera@eduroamnat4-4.eduroamnat.uv.es>
2024-11-27 14:06:08 +0100

    further cleaning and adaptation to WafflesAnalysis

--- src/waffles/np04_analysis/LED_calibration/analysis.py
+++ src/waffles/np04_analysis/LED_calibration/analysis.py
@@ -13,60 +13,79 @@ class analysis(WafflesAnalysis):
     ##################################################################
     def arguments(self, parse: argparse.ArgumentParser):                
 
-        parse.add_argument('-a','--apa',    type=int,   required=True,  help="APA number")
-        parse.add_argument('-p','--pde',    type=float, required=True,  help="photon detection efficiency")
-        parse.add_argument('-b','--batch',  type=int,   required=True,  help="calibration batch")
-
+        parse.add_argument('-a','--apa',      type=int,   required=True,  help="APA number")
+        parse.add_argument('-p','--pde',      type=float, required=True,  help="photon detection efficiency")
+        parse.add_argument('-b','--batch',    type=int,   required=True,  help="calibration batch")
+        parse.add_argument('-ns','--noshow',  action="store_true",        help="do not show figures")
 
     ##################################################################
     def initialize(self, args):                
 
         # store the user arguments into data members
-        self.apa_no = args['apa']
+        self.show_figures = args['noshow']
+        self.apa    = args['apa']
         self.pde    = args['pde']
         self.batch  = args['batch']
         self.data_folderpath = self.path_to_input_file
 
         # calibration configurations 
-        self.run_to_config_      =      run_to_config[self.batch][self.apa_no][self.pde]  # runs for a given configuration
-        self.config_to_channels_ = config_to_channels[self.batch][self.apa_no][self.pde]  # channels for each run
+        #self.run_to_config_      =      run_to_config[self.batch][self.apa][self.pde]  # runs for a given configuration
+        #self.config_to_channels_ = config_to_channels[self.batch][self.apa][self.pde]  # channels for each run
         self.excluded_channels_  =  excluded_channels[self.batch]
+
+        # self.read_input_loop = [[2,2,0.4],
+        #                         [2,2,0.45],
+        #                         [2,2,0.5],
+        #                         [2,3,0.4],
+        #                         [2,3,0.45],
+        #                         [2,3,0.5],
+        #                         [2,4,0.4],
+        #                         [2,4,0.45],
+        #                         [2,2,0.5],
+        #                         [3,2,0.4],
+        #                         [3,2,0.45],
+        #                         [3,2,0.5],
+        #                         [3,3,0.4],
+        #                         [3,3,0.45],
+        #                         [3,3,0.5],
+        #                         [3,4,0.4],
+        #                         [3,4,0.45],
+        #                         [3,2,0.5]                                                                                            
+        #                         ]
 
     ##################################################################
     def read_input(self):
 
+        # self.batch = self.read_input_itr[0]
+        # self.apa   = self.read_input_itr[1]
+        # self.pde   = self.read_input_itr[2]
+
         first = True
         self.wfset = None
+
         # loop over runs
-        for run in self.run_to_config_.keys():
+        runs = run_to_config[self.batch][self.apa][self.pde]
+        for run in runs.keys():
             # loop over endpoints using that run for calibration
-            for endpoint in self.config_to_channels_[self.run_to_config_[run]].keys():
+            channels_and_endpoints = config_to_channels[self.batch][self.apa][self.pde][runs[run]]
+            for endpoint in channels_and_endpoints.keys():
                 
                 # list of channels in that endpoint using that run for calibration
-                channels = self.config_to_channels_[self.run_to_config_[run]][endpoint]
+                channels = channels_and_endpoints[endpoint]
 
-                # data folder for that run, and the batch, apa and pde specified in params.py
-                input_filepath = led_utils.get_input_filepath(self.data_folderpath, self.batch, run, self.apa_no, self.pde)
-
-                # read all files for the given run
-                new_wfset = led_utils.read_data(input_filepath, self.batch, self.apa_no, 0.01, is_folder=False)
-
-                # keep only waveforms in the necessary endpoint and channels
-                new_wfset = led_utils.get_wfset_in_channels(new_wfset, endpoint, channels)
-                
-                # get parameters input for the analysis of the waveforms
-                input_parameters = led_utils.get_analysis_params()
-                checks_kwargs = IPDict()
-                checks_kwargs['points_no'] = new_wfset.points_per_wf
-
-                print("\n Now analysing waveforms from:")
+                print("\n Now loading waveforms from:")
                 print(f" - run {run}")
                 print(f" - endpoint {endpoint}")
-                print(f" - channels {channels} \n")      
+                print(f" - channels {channels} \n")         
+                
+                # data folder for that run, and the batch, apa and pde specified in params.py
+                input_filepath = led_utils.get_input_filepath(self.data_folderpath, self.batch, run, self.apa, self.pde)
 
-                # analise the waveform: compute baseline, integral and amplitud
-                _ = new_wfset.analyse(analysis_label, BasicWfAna, input_parameters, *[],  # *args,
-                    analysis_kwargs={}, checks_kwargs=checks_kwargs, overwrite=True,)
+                # read all files for the given run
+                new_wfset = led_utils.read_data(input_filepath, self.batch, self.apa, 0.01, is_folder=False)
+
+                # keep only waveforms in the necessary endpoint and channels
+                new_wfset = led_utils.get_wfset_in_channels(new_wfset, endpoint, channels)                     
                 
                 if first:
                     self.wfset = new_wfset
@@ -74,14 +93,27 @@ class analysis(WafflesAnalysis):
                 else:
                     self.wfset.merge(new_wfset)
 
-    ##################################################################
-    def analize(self):
+        return True
 
+    ##################################################################
+    def analyze(self):
+
+        """ ------------- analyse the waveform set --------------- """
+
+        # get parameters input for the analysis of the waveforms
+        input_parameters = led_utils.get_analysis_params()
+        checks_kwargs = IPDict()
+        checks_kwargs['points_no'] = self.wfset.points_per_wf
+    
+        # analise the waveform: compute baseline, integral and amplitud
+        _ = self.wfset.analyse(analysis_label, BasicWfAna, input_parameters, *[],  # *args,
+            analysis_kwargs={}, checks_kwargs=checks_kwargs, overwrite=True,)
+                
         """ ------------- Compute charge histogram --------------- """
 
         # Create a grid of WaveformSets for each channel in one APA, and compute the charge histogram
         # Julio suggest decoupling creating the grid and creating the calib histo
-        grid_apa = ChannelWsGrid(APA_map[self.apa_no], self.wfset, compute_calib_histo=True, 
+        grid_apa = ChannelWsGrid(APA_map[self.apa], self.wfset, compute_calib_histo=True, 
                                 bins_number=nbins, domain=np.array((-10000.0, 50000.0)),
                                 variable="integral",analysis_label=None,)
 
@@ -98,16 +130,20 @@ class analysis(WafflesAnalysis):
                             analysis_label=analysis_label, plot_peaks_fits=True,
                             detailed_label=False, verbose=True,)
 
-        title = f"APA {self.apa_no} - Runs {list(self.wfset.runs)}"
+        title = f"APA {self.apa} - Runs {list(self.wfset.runs)}"
         figure.update_layout(title={"text": title,"font": {"size": 24}}, width=1100, height=1200, showlegend=True,)
-        figure.show()
 
-        # figure.write_image(f"{plots_saving_filepath}/apa_{self.apa_no}_calibration_histograms.png")
+        if not self.show_figures:
+            figure.show()
+
+        # figure.write_image(f"{plots_saving_filepath}/apa_{self.apa}_calibration_histograms.png")
 
         """ ------------- Compute gain and S/N ------------- """
 
         # compute gain and S/N for the grid
-        self.output_data = led_utils.get_gain_and_sn(grid_apa, self.excluded_channels_[self.apa_no][self.pde])
+        self.output_data = led_utils.get_gain_and_sn(grid_apa, self.excluded_channels_[self.apa][self.pde])
+
+        return True
 
     ##################################################################
     def write_output(self):
