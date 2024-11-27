303838242b47bfba57019eabe7b5702478d68042
Anselmo Cervera Villanueva <acervera@eduroamnat4-4.eduroamnat.uv.es>
2024-11-27 13:55:37 +0100

    reorganization for WafflesAnalysis class

---
+++ src/waffles/np04_analysis/tau_slow_convolution/imports.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pickle
+import os
+import argparse
+
+from waffles.data_classes.WaveformSet import WaveformSet
+from waffles.input.pickle_file_reader import WaveformSet_from_pickle_file
+from waffles.np04_analysis.tau_slow_convolution.extractor_waveforms import Extractor
+from waffles.np04_data.tau_slow_runs.load_runs_csv import ReaderCSV
+from waffles.data_classes.WafflesAnalysis import WafflesAnalysis as WafflesAnalysis
+from waffles.utils.ConvFitter import ConvFitter
+
+from waffles.utils.baseline.baseline import SBaseline
+
+
