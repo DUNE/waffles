dcb1d16b595bae8b163309f1360298ef79f29045
manuel <matheos.mc2@gmail.com>
2024-12-13 11:31:28 +0100

    improving naming of the example readers

--- test/wtest_hdf5_reader.py
+++ test/wtest_hdf5_reader.py
@@ -1,14 +1,16 @@
 import pickle
+import re
 import waffles.input.raw_hdf5_reader as reader
 
-print("Ok")
 rucio_filepath = "/eos/experiment/neutplatform/protodune/dune/hd-protodune/1a/ec/np04hd_raw_run030003_0000_dataflow0_datawriter_0_20241014T152553.hdf5"
-#filepaths = reader.get_filepaths_from_rucio(rucio_filepath)
-#print(filepaths)
 
+match = re.search(r"run(\d{6})", rucio_filepath)
 
-wfset = reader.WaveformSet_from_hdf5_file( rucio_filepath,                     # path to the root file
-                                           read_full_streaming_data = False, # self-triggered (False) data
-                                         )                                   # subsample the data reading (read each 2 entries)
-with open("wfset_30003.pkl", "wb") as f:
-    pickle.dump(wfset, f)
+wfset = reader.WaveformSet_from_hdf5_file(
+  rucio_filepath,                    
+  read_full_streaming_data=False     
+)
+
+output_filename = f"wfset_{run_number}.pkl"
+with open(output_filename, "wb") as f:
+  pickle.dump(wfset, f)
--- test/wtest_hdf5_reader_2.py
+++ test/wtest_hdf5_reader_2.py
@@ -1,22 +1,30 @@
+import os
 import pickle
+import re
 import waffles.input.raw_hdf5_reader as reader
 
-print("Ok")
+rucio_filepath = (
+    "/eos/experiment/neutplatform/protodune/experiments/"
+    "ProtoDUNE-II/PDS_Commissioning/waffles/1_rucio_paths/027367.txt"
+)
 
-rucio_filepath = "/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-II/PDS_Commissioning/waffles/1_rucio_paths/027367.txt"
+match = re.search(r"(\d{5,6})\.txt", rucio_filepath)
 
 filepaths = reader.get_filepaths_from_rucio(rucio_filepath)
-#print(filepaths)
-
-
-count = 0
 
 for fp in filepaths:
-  print(fp)
-  wfset = reader.WaveformSet_from_hdf5_file( fp,                     # path to the root file
-                                             read_full_streaming_data = False, # self-triggered (False) data
-                                            )
-  
-  with open("/eos/home-f/fegalizz/public/to_Anna/wfset_27367_"+str(count)+".pkl", "wb") as f:
-      pickle.dump(wfset, f)
-  count += 1
+    print(f"Processing file: {fp}")
+    wfset = reader.WaveformSet_from_hdf5_file(
+        fp,                              
+        read_full_streaming_data=False 
+    )
+
+    base_name = os.path.basename(fp).replace(".hdf5", "")
+    output_filename = (
+        f"/eos/home-f/fegalizz/public/to_Anna/wfset_{run_number}_{base_name}.pkl"
+    )
+
+    with open(output_filename, "wb") as f:
+        pickle.dump(wfset, f)
+
+    print(f"WaveformSet saved to {output_filename}")
