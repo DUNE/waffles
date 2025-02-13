eed8093e936cb37685b1d92d75547dfbbaabd2e4
fgalizzi <fedegal99@gmail.com>
2025-02-13 14:39:23 +0100

    Noise analysis almost migrated + moke files

--- src/waffles/np04_analysis/noise_studies/Analysis1.py
+++ src/waffles/np04_analysis/noise_studies/Analysis1.py
@@ -35,8 +35,12 @@ class Analysis1(WafflesAnalysis):
         self.filepath_folder  = input_parameters.filepath_folder
         self.run_vgain_dict   = input_parameters.run_vgain_dict
         self.channel_map_file = input_parameters.channel_map_file
+        df = pd.read_csv(self.channel_map_file, sep=",")
+        self.daphne_channels = df['daphne_ch'].values + 100*df['endpoint'].values
+        self.daphne_to_offline = dict(zip(self.daphne_channels, df['offline_ch']))
         self.out_writing_mode = input_parameters.out_writing_mode
         self.out_path         = input_parameters.out_path
+        self.out_csv_file     = open(self.out_path+"Noise_Studies_Results.csv", self.out_writing_mode)
         self.full_stat        = input_parameters.full_stat
         self.runs             = input_parameters.user_runs
         if (len(self.runs) == 0):
@@ -45,11 +49,25 @@ class Analysis1(WafflesAnalysis):
                 print("No runs to analyze")
                 exit()
 
+        # read_input loop will iter over runs
+        self.read_input_loop = self.runs
+
+        # analyze loop will iter over runs
+        self.analyze_loop = self.runs
+
+
+
     #################################################################
     def read_input(self) -> bool:
         """
         comment
         """
+        self.run = self.read_input_itr
+        
+        print("Reading run: ", self.run)
+        self.wfset_run = nf.read_waveformset(
+                self.filepath_folder, int(self.run), full_stat=self.full_stat)
+                                                                                                                                                                                                                                                                                                                                                                                                                        s                           c 
         return True
 
 
@@ -58,76 +76,27 @@ class Analysis1(WafflesAnalysis):
         """
         comment
         """
-        return True
-    
+        self.eps = []
+        self.chs = []
+        self.offline_chs = []
+        self.rms = []
+        self.fft2_avgs = []
+        self.vgains = []
 
-    #################################################################
-    def write_output(self) -> bool:
-        """
-        comment
-        """
-        return True
+        ep_ch_dict = self.wfset_run.get_run_collapsed_available_channels()
 
-    #################################################################
-# --- MAIN ----------------------------------------------------------
-if __name__ == "__main__":
-
-    # Setup variables according to the noise_run_info.yaml file
-    with open("./noise_run_info.yaml", 'r') as stream:
-        run_info = yaml.safe_load(stream)
-
-    filepath_folder  = run_info.get("filepath_folder")
-    run_vgain_dict   = run_info.get("run_vgain_dict", {})
-    channel_map_file = run_info.get("channel_map_file")
-
-    # Setup variables according to the user_config.yaml file
-    with open("user_setting.yaml", 'r') as stream:
-        user_config = yaml.safe_load(stream)
-
-    out_writing_mode = user_config.get("out_writing_mode")
-    out_path  = user_config.get("out_path")
-    full_stat = user_config.get("full_stat")
-    runs      = user_config.get("user_runs", [])
-    if (len(runs) == 0):
-        runs = user_config.get("all_noise_runs", [])
-        if (len(runs) == 0):
-            print("No runs to analyze")
-            exit()
-
-    # Read the channel map file (daphne ch <-> offline ch)
-    df = pd.read_csv(channel_map_file, sep=",")
-    daphne_channels = df['daphne_ch'].values + 100*df['endpoint'].values
-    daphne_to_offline = dict(zip(daphne_channels, df['offline_ch']))
-
-
-    # File where the results will be printed (run, vgain, endpoint, channel, offline_channel, rms)
-    my_csv_file = open(out_path+"Noise_Studies_Results.csv", out_writing_mode)
-
-    # --- LOOP OVER RUNS ----------------------------------------------
-    for run in runs:
-        print("Reading run: ", run)
-        wfset_run = nf.read_waveformset(filepath_folder, run, full_stat=full_stat)
-        endpoints = wfset_run.get_set_of_endpoints()
-
-        # --- LOOP OVER ENDPOINTS -------------------------------------
-        for ep in endpoints:
-            print("Endpoint: ", ep)
-            wfset_ep = waffles.WaveformSet.from_filtered_WaveformSet(wfset_run, nf.allow_ep_wfs, ep)
-
-            ep_ch_dict = wfset_ep.get_run_collapsed_available_channels()
+        for ep in ep_ch_dict:
             channels = list(ep_ch_dict[ep])
+            wfset_ep = waffles.WaveformSet.from_filtered_WaveformSet(self.wfset_run, nf.allow_ep_wfs, ep)
 
-            # --- LOOP OVER CHANNELS ----------------------------------
             for ch in channels:
-                print("Channel: ", ch)
                 wfset_ch = waffles.WaveformSet.from_filtered_WaveformSet(wfset_ep, nf.allow_channel_wfs, ch)
-                # check if the channel is in the daphne_to_offline dictionary
                 channel = np.uint16(np.uint16(ep)*100+np.uint16(ch))
-                if channel not in daphne_to_offline:
+                if channel not in self.daphne_to_offline:
                     print(f"Channel {channel} not in the daphne_to_offline dictionary")
                     continue
-                offline_ch = daphne_to_offline[channel]
-        
+                offline_ch = self.daphne_to_offline[channel]
+
                 wfs = wfset_ch.waveforms
                 nf.create_float_waveforms(wfs)
                 nf.sub_baseline_to_wfs(wfs, 1024)
@@ -135,7 +104,7 @@ if __name__ == "__main__":
                 norm = 1./len(wfs)
                 fft2_avg = np.zeros(1024)
                 rms = 0.
-
+                
                 # Compute the average FFT of the wfs.adcs_float
                 for wf in wfs:
                     rms += np.std(wf.adcs_float)
@@ -145,11 +114,28 @@ if __name__ == "__main__":
 
                 fft2_avg = fft2_avg*norm
                 rms = rms*norm
-                vgain = run_vgain_dict[run]
-                
-                # print run, vgain, ep, ch, offline_ch, rms in a csv file
-                my_csv_file.write(f"{run},{vgain},{ep},{ch},{offline_ch},{rms}\n")
-                # print the FFT in a txt file
-                np.savetxt(out_path+"/FFT_txt/fft_run_"+str(run)+"_vgain_"
-                           +str(vgain)+"_ch_"+str(channel)+"_offlinech_"
-                           +str(offline_ch)+".txt", fft2_avg[0:513])
+                vgain = self.run_vgain_dict[self.run]
+
+                self.eps.append(ep)
+                self.chs.append(ch)
+                self.offline_chs.append(offline_ch)
+                self.rms.append(rms)
+                self.fft2_avgs.append(fft2_avg[0:513])
+                self.vgains.append(vgain)
+
+
+        return True
+    
+
+    #################################################################
+    def write_output(self) -> bool:
+        """
+        comment
+        """
+        for i in range(len(self.eps)):
+            self.out_csv_file.write(f"{self.run},{self.vgains[i]},{self.eps[i]},{self.chs[i]},{self.offline_chs[i]},{self.rms[i]}\n")
+            np.savetxt(self.out_path+"/FFT_txt/fft_run_"+str(self.run)+"_vgain_"+str(self.vgains[i])
+                       +"_ch_"+str(self.chs[i])+"_offlinech_"+str(self.offline_chs[i])
+                       +".txt", self.fft2_avgs[i])
+
+        return True
--- src/waffles/np04_analysis/noise_studies/imports.py
+++ src/waffles/np04_analysis/noise_studies/imports.py
@@ -4,5 +4,7 @@ import numpy as np
 import pandas as pd
 #import noisy_function as nf
 import utils as nf
+from typing import List, Dict, Any
 
 import waffles
+from waffles.data_classes.WafflesAnalysis import WafflesAnalysis as WafflesAnalysis, BaseInputParams
