4cb2b7e9925aec9a5334b4ebfec33042504e0120
Julio Urena <julio.urena@ific.uv.es>
2025-02-21 20:46:37 +0100

    Add missing imports in example_analysis/imports.py

--- src/waffles/np04_analysis/example_analysis/imports.py
+++ src/waffles/np04_analysis/example_analysis/imports.py
@@ -3,6 +3,8 @@ import numpy as np
 import pickle
 from matplotlib import pyplot as plt
 from scipy import signal as spsi
-import waffles.input_output.WaveformAdcs as WaveformAdcs
+from pydantic import Field
+from waffles.data_classes.WafflesAnalysis import WafflesAnalysis, BaseInputParams
+from waffles.data_classes.WaveformAdcs import WaveformAdcs
 import waffles.input_output.raw_hdf5_reader as reader
 import waffles.np04_analysis.example_analysis.utils as utils
