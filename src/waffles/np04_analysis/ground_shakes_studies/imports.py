94691aed487e3aaa80a870ea349c174b146ccc3a
Valeria Trabattoni <vtrabatt@lxplus985.cern.ch>
2025-03-11 11:50:14 +0100

    per avere i plot

--- src/waffles/np04_analysis/ground_shakes_studies/imports.py
+++ src/waffles/np04_analysis/ground_shakes_studies/imports.py
@@ -6,6 +6,19 @@ from scipy import signal as spsi
 from pydantic import Field, field_validator
 from waffles.data_classes.WafflesAnalysis import WafflesAnalysis, BaseInputParams
 from waffles.data_classes.WaveformAdcs import WaveformAdcs
-import waffles.input_output.raw_hdf5_reader as reader
 import waffles.np04_analysis.example_analysis.utils as wnu
 import waffles.core.utils as wcu
+
+from waffles.input_output.hdf5_file_reader import WaveformSet_from_hdf5_file
+
+import plotly.graph_objects as pgo
+from waffles.plotting.plot import plot_ChannelWsGrid
+import plotly.subplots as psu
+import gc
+import h5py
+
+from waffles.data_classes.BasicWfAna import BasicWfAna
+from waffles.data_classes.IPDict import IPDict
+from waffles.np04_data.ProtoDUNE_HD_APA_maps import APA_map
+from waffles.data_classes.ChannelWsGrid import ChannelWsGrid
+from waffles.np04_analysis.ground_shakes_studies import utils as gs_utils
