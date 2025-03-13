b9d702845f2ef85ba337b7346b54be0ae4d4aeb0
andrearf28 <arochefe@lxplus989.cern.ch>
2025-03-13 18:28:42 +0100

    Adding more tools for the plots

--- src/waffles/np04_analysis/ground_shakes/scripts/plot_grid.py
+++ src/waffles/np04_analysis/ground_shakes/scripts/plot_grid.py
@@ -3,7 +3,7 @@
 import waffles.plotting.drawing_tools as draw
 wset = draw.read("../data/wfset_10_30201.hdf5",0,1)
 
-
+'''
 # -------- Time offset histograms --------
 draw.plot_function_grid(wset, apa=2,tmin=-1000,tmax=1000,  plot_function=draw.plot_to_function)
 input()
@@ -14,3 +14,4 @@ input()
 
 # ----------- Sigma histograms -----------
 draw.plot_function_grid(wset, apa=2,tmin=-1000,tmax=1000,  plot_function=draw.plot_sigma_function)
+'''
