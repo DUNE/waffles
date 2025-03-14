df3401b20d90a83e4bff881e8f721b2714dd984e
Valeria Trabattoni <vtrabatt@lxplus900.cern.ch>
2025-03-14 14:22:45 +0100

    test

---
+++ src/waffles/np04_analysis/ground_shakes/scripts/load.py
@@ -0,0 +1,5 @@
+
+import waffles.plotting.drawing_tools as draw
+from waffles.data_classes.WaveformSet import WaveformSet
+wset = draw.read("/afs/cern.ch/work/v/vtrabatt/fddaq-v5.2.1-a9/waffles/scripts/processed_merged_run_26701.hdf5",0,1)
+
