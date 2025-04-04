af37f2f2de9b43341111eb88f5ba84e5b14cd623
Francesca Alemanno <francesca.alemanno@le.infn.it>
2025-04-04 12:48:20 +0200

    updated file list and removed import ROOT

--- src/waffles/np04_analysis/light_vs_hv/imports.py
+++ src/waffles/np04_analysis/light_vs_hv/imports.py
@@ -7,7 +7,6 @@ import importlib
 from pathlib import Path
 from plotly import graph_objects as pgo
 from plotly.subplots import make_subplots
-import ROOT as root
 from scipy.optimize import curve_fit
 from scipy.fft import fft, fftshift
 from scipy import signal
