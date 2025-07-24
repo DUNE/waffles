26dcc60ff6edc218c23e6b8f7597ba0bc3877c87
hvsouza <henriquevieira.souza@gmail.com>
2025-07-24 14:31:23 +0200

    Minor impromevements for ipynb

--- src/waffles/np02_utils/PlotUtils.py
+++ src/waffles/np02_utils/PlotUtils.py
@@ -132,7 +132,7 @@ def plot_grid(chgrid: ChannelWsGrid, title:str = "", html: Union[Path, None] = N
                            **kwargs
                            )
     else:
-        plot_CustomChannelGrid(chgrid, plot_function, figure=fig, wf_func=kwargs.pop("doprocess", None), **kwargs)
+        plot_CustomChannelGrid(chgrid, plot_function, figure=fig, wf_func=kwargs.pop("wf_func", None), **kwargs)
 
     fig.update_layout(title=title, template="plotly_white",
                       width=1000, height=800, showlegend=True)
@@ -146,23 +146,35 @@ def plot_grid(chgrid: ChannelWsGrid, title:str = "", html: Union[Path, None] = N
 # ╰────────────────────────────────────────────────────────────────────────────╯
 
 
-def genhist(wfset:WaveformSet, figure:go.Figure, row, col):
-    values = [wf.analyses["std"].result['integral'] for wf in wfset.waveforms]
+def genhist(wfset:WaveformSet, figure:go.Figure, row, col, wf_func = None):
+    variabletype = wf_func.get('variable', 'integral') if wf_func is not None else 'integral'
+    values = [wf.analyses["std"].result[variabletype] for wf in wfset.waveforms]
     bins = np.linspace(-50e3, 50e3, 100)
+    if wf_func is not None:
+        if 'bins' in wf_func:
+            bins = wf_func['bins']
+    counts, edges = np.histogram(values, bins=bins)
 
     figure.add_trace(
-        go.Histogram(
-            x=values,
-            xaxis='x',
-            xbins=dict(start=bins[0], end=bins[-1], size=(bins[1] - bins[0])),
-            autobinx=False,
-
+        go.Scatter(  
+            x=edges,
+            y=counts,
+            mode='lines',
+            line=dict(  
+                color='black', 
+                width=0.5,
+                shape='hv'),
         ),
         row=row, col=col
     )
 
 
-def fithist(wfset:WaveformSet, figure:go.Figure, row, col, doprocess=lambda: False):
+def fithist(wfset:WaveformSet, figure:go.Figure, row, col, wf_func = None):
+    doprocess = wf_func.get("doprocess", True) if wf_func is not None else True
+    dofit = wf_func.get("dofit", True) if wf_func is not None else True
+    variable = wf_func.get('variable', 'integral') if wf_func is not None else 'integral'
+    show_progress = wf_func.get('show_progress', False) if wf_func is not None else False
+    
 
     params = ch_read_params()
     endpoint = wfset.waveforms[0].endpoint
@@ -171,20 +183,29 @@ def fithist(wfset:WaveformSet, figure:go.Figure, row, col, doprocess=lambda: Fal
     if endpoint not in params or channel not in params[endpoint]:
         raise ValueError(f"No parameters found for endpoint {endpoint} and channel {channel} in the configuration file.")
 
+    if(len(wfset.available_channels[list(wfset.runs)[0]][endpoint]) > 1):
+        raise ValueError(f"Should have only one channel in the waveform set...")
     
-    if doprocess():
+    if doprocess:
         runBasicWfAnaNP02(wfset,
                           int_ll=params[endpoint][channel]['fit'].get('int_ll', 254),
                           int_ul=params[endpoint][channel]['fit'].get('int_ul', 270),
-                          amp_ll=params[endpoint][channel]['fit'].get('amp_ll', 250),
-                          amp_ul=params[endpoint][channel]['fit'].get('amp_ul', 280),
-                          show_progress=False
+                          amp_ll=params[endpoint][channel]['fit'].get('amp_ll', 254),
+                          amp_ul=params[endpoint][channel]['fit'].get('amp_ul', 270),
+                          show_progress=show_progress,
                           )
 
     bins_int = params[endpoint][channel]['fit'].get('bins_int', 100)
     domain_int_str = params[endpoint][channel]['fit'].get('domain_int', [-10e3, 100e3])
+
+
     domain_int = [float(x) for x in domain_int_str]
     domain_int = np.array(domain_int)
+    if wf_func is not None:
+        if 'bins' in wf_func:
+            tbins = wf_func['bins']
+            bins_int = len(tbins)
+            domain_int = np.array([tbins[0], tbins[-1]])
 
     max_peaks = params[endpoint][channel]['fit'].get('max_peaks', 3)
     prominence = params[endpoint][channel]['fit'].get('prominence', 0.15)
@@ -197,17 +218,27 @@ def fithist(wfset:WaveformSet, figure:go.Figure, row, col, doprocess=lambda: Fal
         wfset,
         bins_number=bins_int,
         domain=domain_int,
-        variable='integral',
+        variable=variable,
         analysis_label = "std",
     )
+
+    if not dofit:
+        plot_CalibrationHistogram(
+            hInt,
+            figure=figure,
+            row=row, col=col,
+            plot_fits=False,
+            name=f"{dict_uniqch_to_module[str(UniqueChannel(wfset.waveforms[0].endpoint, wfset.waveforms[0].channel))]}",
+        )
+        return
 
     # This method in case histogram should cut average
     # average_hits = hInt.edges[:-1]*hInt.counts
     # average_hits = np.sum(average_hits)/np.sum(hInt.counts)
 
     # This method in case histogram should not cut average
-    integral_sum = np.nansum( np.array([ wf.analyses["std"].result['integral'] for wf in wfset.waveforms ]) )
-    n_integrals = np.sum( [1 if wf.analyses["std"].result['integral'] is not np.nan else 0 for wf in wfset.waveforms ] )
+    integral_sum = np.nansum( np.array([ wf.analyses["std"].result[variable] for wf in wfset.waveforms ]) )
+    n_integrals = np.sum( [1 if wf.analyses["std"].result[variable] is not np.nan else 0 for wf in wfset.waveforms ] )
     average_hits = integral_sum / n_integrals if n_integrals > 0 else 0
 
     fit_hist = fit_peaks_of_CalibrationHistogram(
@@ -236,7 +267,7 @@ def fithist(wfset:WaveformSet, figure:go.Figure, row, col, doprocess=lambda: Fal
         gain = spe_charge - zero_charge
         snr = gain / baseline_stddev
     except:
-        print(f"Could fit for {dict_uniqch_to_module[str(UniqueChannel(wfset.waveforms[0].endpoint, wfset.waveforms[0].channel))]}")
+        print(f"Could not fit for {dict_uniqch_to_module[str(UniqueChannel(wfset.waveforms[0].endpoint, wfset.waveforms[0].channel))]}")
 
     plot_CalibrationHistogram(
         hInt,
