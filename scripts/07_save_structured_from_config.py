956923284a6bff00881c36c4b6fc032ca195dcc1
Maritza Juliette Delgado Gonzalez <delgadog@lxplus943.cern.ch>
2025-08-14 15:48:42 +0200

    Added labels for PMT processing

--- scripts/07_save_structured_from_config.py
+++ scripts/07_save_structured_from_config.py
@@ -236,6 +236,9 @@ def main(config):
         elif detector == 'VD_Cathode_PDS':
             suffix="cathode"
             extra="_cathode"
+        elif detector == 'VD_PMT_PDS':    
+            suffix="pmt"
+            extra="_pmt"
         config_data["suffix"] = suffix
 
 
