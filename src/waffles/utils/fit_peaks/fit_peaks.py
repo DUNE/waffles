628c832c09743746e1b8ca857008a8d0603f2cee
Gloria Piemonti <g.piemonti@campus.unimib.it>
2025-08-28 14:58:38 +0200

    Updatades fit parameters and limit for baseline

--- src/waffles/utils/fit_peaks/fit_peaks.py
+++ src/waffles/utils/fit_peaks/fit_peaks.py
@@ -285,7 +285,8 @@ def fit_peaks_of_CalibrationHistogram(
 
     for p in mm.parameters: # we are now free
         mm.fixed[p] = False
-        mm.limits[p] = (1e-6, None) # They are all positive btw
+        if p != "mean_baseline":
+            mm.limits[p] = (1e-6, None) # They are all positive btw
     mm.migrad() # second call to ensure convergence now with free parameters
     mm.migrad() # try hard call 
     mm.hesse() # compute errors
