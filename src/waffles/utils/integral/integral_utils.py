2ccb065198c74d57e3de4bfc03dc04ead50289cd
Julio <jurenag@outlook.es>
2025-10-14 11:14:44 +0200

    Add `get_zero_crossing_upper_limit` parameter to get_pulse_window_limits

--- src/waffles/utils/integral/integral_utils.py
+++ src/waffles/utils/integral/integral_utils.py
@@ -8,18 +8,24 @@ def get_pulse_window_limits(
     baseline: float,
     deviation_from_baseline: float,
     lower_limit_correction: int = 0,
-    upper_limit_correction: int = 0
+    upper_limit_correction: int = 0,
+    get_zero_crossing_upper_limit: bool = False
 ) -> Tuple[int, int]:
     """This function takes an unidimensional numpy array
     representing the pulse signal, in ADCs, coming from an SiPM.
     It returns a tuple of two integers representing the lower
     and upper limits, respectively, of the SiPM pulse in the
-    given array. The lower limit is computed as the first point
-    which negatively deviates from the given baseline by a
-    certain given amount. The upper limit is computed as the
-    last point, after the minimum of the pulse, which deviates
-    from the given baseline by more than the given amount.
-    It is assumed that the pulse is negative.
+    given array. The lower limit is unconditionally computed
+    as the first point which negatively deviates from the given
+    baseline by a certain given amount. The calculation of the
+    upper limit depends on the get_zero_crossing_upper_limit
+    parameter. If it set to False, then the upper limit is
+    computed as the last point, after the minimum of the pulse,
+    which deviates from the given baseline by more than the
+    given amount. If it is set to True, then the upper limit
+    is computed as the last point, after the minimum of the
+    pulse, which stays below the baseline. Note that it is
+    assumed that the pulse is negative.
 
     Parameters
     ----------
@@ -32,20 +38,35 @@ def get_pulse_window_limits(
         It must be greater than 0.0 and smaller than 1.0. It is
         interpreted as a fraction of baseline - np.min(adcs_array).
         I.e. the lower limit of the pulse window is assumed to
-        be the first point which drops below x, while the upper
-        limit is assumed to be the last point, after the
-        minimum of the pulse, which stays below x, where
+        be the first point which drops below x, where 
 
             x = baseline - \
                 (deviation_from_baseline * (baseline - np.min(adcs_array))).
 
-    lower_limit_correction (resp. upper_limit_correction): int
+        Additionally, if get_zero_crossing_upper_limit is False,
+        then the upper limit is assumed to be the last point, after
+        the minimum of the pulse, which stays below x.
+    lower_limit_correction: int
         This is an optional parameter that can be used to apply
-        a correction, to the lower (resp. upper) limit of the
-        pulse window. I.e. if this parameter is set to N, then
-        the final iterator value for the lower (resp. upper)
-        limit is the one inferred by the deviation-from-baseline
-        analysis plus N. Note that N may be positive or negative.
+        a correction, to the lower limit of the pulse window. I.e.
+        if this parameter is set to N, then the final iterator
+        value for the lower limit is the one inferred by the
+        deviation-from-baseline analysis plus N. Note that N may
+        be positive or negative.
+    upper_limit_correction: int
+        This parameter only makes a difference if
+        get_zero_crossing_upper_limit is set to False. In that
+        case, it is interpreted in the same way as
+        lower_limit_correction, but it is applied to the upper
+        limit of the pulse window.
+    get_zero_crossing_upper_limit: bool
+        If set to False, the upper limit of the pulse window
+        is computed as the last point, after the minimum of
+        the pulse, which deviates from the given baseline by
+        more than the given amount. If it is set to True, then
+        the upper limit is computed as the last point, after
+        the minimum of the pulse, which stays below the
+        baseline.
 
     Returns
     -------
@@ -94,31 +115,46 @@ def get_pulse_window_limits(
     # or equal to lower_limit is also guaranteed.
 
     upper_limit = None
-    for i in range(idx_min+1, len(adcs_array)):
-        if adcs_array[i] > threshold:
-            upper_limit = i - 1
-            break
+    if not get_zero_crossing_upper_limit:
+        for i in range(idx_min + 1, len(adcs_array)):
+            if adcs_array[i] > threshold:
+                upper_limit = i - 1
+                break
+    else:
+        for i in range(idx_min + 1, len(adcs_array)):
+            if adcs_array[i] > baseline:
+                upper_limit = i - 1
+                break
 
     if upper_limit is None:
+        exception_reason = "The upper limit of the pulse window "\
+        "could not be found. I.e. the signal never raised above the "
+
+        if not get_zero_crossing_upper_limit:
+            exception_reason += f"threshold ({threshold}) "
+        else:
+            exception_reason += f"baseline ({baseline}) "
+
+        exception_reason += f"after reaching its minimum value "\
+        f"({adcs_array[idx_min]})."
+
         raise Exception(
             GenerateExceptionMessage(
                 3,
                 'get_pulse_window_limits()',
-                "The upper limit of the pulse window could not be "
-                "found. I.e. the signal never raised above the threshold "
-                f"({threshold}) after reaching its minimum value "
-                f"({adcs_array[idx_min]})."
+                exception_reason
             )
         )
 
     # Apply the corrections to the limits
     lower_limit += lower_limit_correction
-    upper_limit += upper_limit_correction
+    if not get_zero_crossing_upper_limit:
+        upper_limit += upper_limit_correction
 
-    # Make sure that the limits are within the bounds of the array
+    # Make sure that the corrected limits are within the bounds of the array
     lower_limit, upper_limit = max(0, lower_limit), max(0, upper_limit)
-    lower_limit, upper_limit = min(len(adcs_array)-1, lower_limit), \
-        min(len(adcs_array)-1, upper_limit)
+    lower_limit, upper_limit = min(len(adcs_array) - 1, lower_limit), \
+        min(len(adcs_array) - 1, upper_limit)
 
     # Make sure that the corrections did not make the lower limit
     # greater than or equal to the upper limit
