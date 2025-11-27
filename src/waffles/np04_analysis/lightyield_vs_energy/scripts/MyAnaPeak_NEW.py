3109ce0582440a6f9cc671ef1b177081ca73c722
Anna-B99 <anna2461999@gmail.com>
2025-11-27 14:15:41 +0100

    starting new renan method to convolv/deconvolve waveforms. In renan_analysi you can find the searching of the peaks working that comes prof MyAnaPeak_NEW and starting the implementation for cutting the full-streaming window

---
+++ src/waffles/np04_analysis/lightyield_vs_energy/scripts/MyAnaPeak_NEW.py
@@ -0,0 +1,123 @@
+# Analysis class to search for peaks and beam event timestamp 
+
+from waffles.data_classes.WaveformAdcs import WaveformAdcs
+from waffles.data_classes.IPDict import IPDict
+from waffles.data_classes.WfAna import WfAna
+from waffles.data_classes.WfAnaResult import WfAnaResult
+
+import waffles.Exceptions as we
+import numpy as np
+from scipy.signal import find_peaks
+
+
+class MyAnaPeak_NEW(WfAna):
+    def __init__(self, input_parameters: IPDict):
+        """BasicWfAna class initializer. 
+        No checks are perfomed here.
+
+        A NEGATIVE signal is assumed!
+
+        Parameters
+        ----------
+        input_parameters: IPDict
+            This IPDict must contain the following keys:
+                - baseline_limits (list of int) -> limits to compute the baseline as mean [start, stop]
+                - n_std (float) -> number of standard deviazione wrt baseline to use to search for peaks
+                - peak_distance (int) -> minimun distance (in timeticks) between two peaks
+                - beam_timeoffset_limits (list of int) -> limits to search for beam events, looking at timeoffset (peak timetick + start waveforms - daq timestamp), usually (-40,-20)
+        """
+
+        self.__baseline_limits = input_parameters['baseline_limits']
+        self.__n_std = input_parameters['n_std']
+        self.__peak_distance = input_parameters['peak_distance']
+        self.__beam_timeoffset_limits = input_parameters['beam_timeoffset_limits']
+
+        super().__init__(input_parameters)
+
+    @property
+    def baseline_limits(self):
+        return self.__baseline_limits
+
+    @property
+    def n_std(self):
+        return self.__n_std
+
+    @property
+    def peak_distance(self):
+        return self.__peak_distance
+    
+    @property
+    def beam_timeoffset_limits(self):
+        return self.__beam_timeoffset_limits
+
+
+    def analyse(self, waveform: WaveformAdcs) -> None:
+        """With respect to the given WaveformAdcs object, this 
+        analyser method does the following:
+          - compute the baseline as simple mean
+          - remove the baseline from the signal, and invert it 
+          - search for peaks (index, peak_time, peak_absolute_time, peak_amplitude)
+          - compute the time_offset (PDS - DAQ) as peak_absolute_time - daq_window_timestamp
+          - select beam peaks 
+
+        Parameters
+        ----------
+        waveform: WaveformAdcs
+            The WaveformAdcs object which will be analysed
+
+        Returns
+        ----------
+        None
+        """
+    
+        # Baseline of first 300 points + removing it and inverting (find_peaks search for positive ones)
+        mean_baseline = np.mean(waveform.adcs[self.__baseline_limits[0] : self.__baseline_limits[1]])
+        clean = mean_baseline - waveform.adcs
+        std = np.std(clean[self.__baseline_limits[0] : self.__baseline_limits[1]])
+
+        # Peak finding 
+        peak_index, properties = find_peaks(
+            clean,
+            height = self.__n_std * std,        
+            prominence = self.__n_std * std,
+            distance = self.__peak_distance)
+        
+        peak_amplitude = mean_baseline-properties['peak_heights']
+
+        x = np.arange(len(waveform.adcs)) 
+        peak_time = x[peak_index]
+        peak_absolute_time = waveform.timestamp + waveform.time_offset + peak_time 
+        time_offset = peak_absolute_time - waveform.daq_window_timestamp
+
+        # Beam selection
+        mask = (time_offset >= self.__beam_timeoffset_limits[0]) & (time_offset <= self.__beam_timeoffset_limits[1])
+        beam_peak_index = peak_index[mask]
+        beam_peak_time = peak_time[mask]
+        beam_peak_amplitude = peak_amplitude[mask]
+        beam_peak_absolute_time = peak_absolute_time[mask]
+        
+        # Results 
+        self._WfAna__result = WfAnaResult(
+            mean_baseline = mean_baseline,
+            peak_index=peak_index,
+            peak_time = peak_time,
+            peak_amplitude = peak_amplitude,
+            peak_absolute_time= peak_absolute_time,
+            beam_peak_index =beam_peak_index,
+            beam_peak_time = beam_peak_time,
+            beam_peak_amplitude = beam_peak_amplitude,
+            beam_peak_absolute_time = beam_peak_absolute_time,
+            time_offset = time_offset
+        )
+        return
+    
+
+    @staticmethod
+    @we.handle_missing_data
+    def check_input_parameters(
+            input_parameters: IPDict,
+            points_no: int
+    ) -> None:
+
+        # No checks for now
+        return
