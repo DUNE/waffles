b619d7c16410c7f7ef65a89e8a6d91b43898144d
hvsouza <henriquevieira.souza@gmail.com>
2025-12-20 15:57:39 +0100

    First version of template retriever. Modified name of current data

--- src/waffles/np02_utils/load_utils.py
+++ src/waffles/np02_utils/load_utils.py
@@ -4,7 +4,9 @@ import copy
 import yaml
 import pandas as pd
 import pathlib
+import re
 from waffles.input_output.hdf5_structured import load_structured_waveformset
+from waffles.np02_utils.AutoMap import dict_module_to_uniqch
 
 def open_processed(run, dettype, datadir, channels = None, endpoints=None, nwaveforms=None, mergefiles = True, verbose=True):
     """
@@ -61,23 +63,41 @@ def ch_show_avaliable_calib_files():
     for f in files:
         print(f"- {f}")
 
-def ch_read_calib(filename: str = 'np02-config-v3.0.0.csv') -> dict:
-    try:
-        with resources.files('waffles.np02_utils.data.calibration_data').joinpath(filename).open('r') as f:
-            df = pd.read_csv(f, skipinitialspace=True)
-            df = df.set_index(['endpoint', 'channel'])[['Gain', 'SpeAmpl']]
-            # now regroup by endpoint
-            nested_dict = {}
-            for values in df.itertuples():
-                ep, ch = getattr(values, 'Index')
-                nested_dict.setdefault(ep, {})[ch] = {'Gain': getattr(values, 'Gain'), 'SpeAmpl': getattr(values, 'SpeAmpl')}
-            return nested_dict
-    except Exception as error:
-        print(error)
-        print("\n\n")
+def ch_read_calib(filename: str = 'np02-config-v3.0.0.csv', attributes = ['Gain', 'SpeAmpl']) -> dict:
+
+    thefile = resources.files('waffles.np02_utils.data.calibration_data').joinpath(filename)
+    if thefile.is_file() is False:
         ch_show_avaliable_calib_files()
-        print("\n\n")
-        
         raise FileNotFoundError(
             f"Could not find the {filename} file in the waffles.np02_utils.PlotUtils.data package.\nWaffles should be installed with -e option to access this file.\n"
-        )
+            )
+
+    with thefile.open('r') as f:
+        df = pd.read_csv(f, skipinitialspace=True)
+        df = df.set_index(['endpoint', 'channel'])[attributes]
+        # now regroup by endpoint
+        nested_dict = {}
+        for values in df.itertuples():
+            ep, ch = getattr(values, 'Index')
+            nested_dict.setdefault(ep, {})[ch] = { k: getattr(values, k) for k in attributes }
+        return nested_dict
+
+def ch_read_template(template_folder:str = 'templates_multi_pe_normalized'):
+    thepath = resources.files('waffles.np02_data.templates').joinpath(template_folder)
+    template_files = [f for f in thepath.iterdir() if f.is_file() and f.name.endswith('.txt')]
+    ret = {}
+    # file name is template_{reference_run}_{module}_{channel}.txt
+    # getting module and channel from file name to create dictionary
+    for tfile in template_files:
+        match = re.match(r'template_\d+_([CM][0-9])_([1,2])\.txt', tfile.name)
+        if not match:
+            raise ValueError(f"Template file name {tfile} does not match expected pattern.")
+
+        modulename = f"{match.group(1)}({match.group(2)})"
+        uch = dict_module_to_uniqch[modulename]
+        endpoint, channel = uch.endpoint, uch.channel
+        with tfile.open('r') as f:
+            template_waveform = pd.read_csv(f, header=None).to_numpy().flatten()
+        ret.setdefault(endpoint, {})[channel] = template_waveform
+
+    return ret
