ed2ea9d9ed5c16282b880d0858db37cf2e1c07f1
Esteban Cristaldo <e.cristaldo27@gmail.com>
2026-04-28 12:32:13 +0000

    preparing to merge main into current branch

--- src/waffles/np04_analysis/vgain_analysis/scripts/xrdcp_test.py
+++ src/waffles/np04_analysis/vgain_analysis/scripts/xrdcp_test.py
@@ -1,12 +1,12 @@
 import os, subprocess
 
 env = os.environ.copy()
-env["BEARER_TOKEN_FILE"] = "/run/user/1000/davs.token"
-env["X509_CERT_DIR"]     = "/etc/grid-security/certificates"
+#env["BEARER_TOKEN_FILE"] = "/run/user/1000/davs.token"
+#env["X509_CERT_DIR"]     = "/etc/grid-security/certificates"
 
 cmd = [
           "xrdcp",
-            "root://ccxrootdegee.in2p3.fr:1094/pnfs/in2p3.fr/data/dune/disk/hd-protodune/43/0b/np04hd_raw_run029876_0000_dataflow0_datawriter_0_20241011T071028.hdf5",
+            "root://eospublic.cern.ch:1094//eos/experiment/neutplatform/protodune/dune/np_buffer/vd-protodune/7a/e0/np02vd_raw_run043191_0000_df-s03-d1_dw_0_20260309T112125.hdf5",
               "./test_from_python.hdf5"
               ]
 
