1402574c4160713d007b64b8c670fe393f886f67
fgalizzi <fedegal99@gmail.com>
2026-04-30 16:40:00 +0200

    Add cb mar 26 channel mapping

--- src/waffles/coldboxVD/utils/utils.py
+++ src/waffles/coldboxVD/utils/utils.py
@@ -28,3 +28,28 @@ def get_cb25_channel_mapping() -> pd.DataFrame:
 
     return df
 
+@lru_cache(maxsize=1)
+def get_cb_march26_channel_mapping() -> pd.DataFrame:
+    """
+    This function reads the Coldbox-March channel mapping file and returns it as a pandas DataFrame.
+    The dataframe contains the following columns:
+    ConfigCh, DAQCh, Module, SiPM, LN2Vbr
+    Parameters
+    ----------
+
+    Returns
+    -------
+    - df: pd.DataFrame
+        The channel mapping as a pandas DataFrame
+    """
+    wafflesdir = Path(waffles.__file__).parent
+    mapping_file = wafflesdir / "coldboxVD" / "utils" / "CB_March26_ChannelMap.csv"
+    if not Path(mapping_file).exists() :
+        print(mapping_file)
+        raise FileNotFoundError(
+            "The channel mapping was not found. You probably need to install waffles with -e option:\n`python3 -m pip install -e .`")
+    
+    df = pd.read_csv(mapping_file, sep=",")
+
+    return df
+
